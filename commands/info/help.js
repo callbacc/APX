d9bde0d1145ce7e49d4a42fdc0fb312681901bd7
callbacc <65503452+callbacc@users.noreply.github.com>
2020-06-13 02:17:46 -0700

    Create help.js

---
+++ commands/info/help.js
@@ -0,0 +1,19 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+    name: "help",
+    aliases: ["bal", "balance", "bank"],
+    category: "fun",
+    description: "Commit a crime.",
+    usage: "[crime]",
+    run: async (client, message, args) => {
+        let embed = new MessageEmbed()
+            .setColor("#00FF00")
+            .setDescription(`Fun: daily, premium, rank, roulette, transfer
+            Info: help, remind
+            Music: loop, np, pause, play, queue, resume, skip, stop, volume
+            Rep: rep, reps
+            Work: coins, crime, work`);
+        message.channel.send(embed);
+    }
+}
