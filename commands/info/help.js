e8cc7d1e9c410d988e4abc8df60bf430cb98829c
callbacc <65503452+callbacc@users.noreply.github.com>
2020-06-13 02:22:40 -0700

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -9,11 +9,11 @@ module.exports = {
     run: async (client, message, args) => {
         let embed = new MessageEmbed()
             .setColor("#00FF00")
-            .setDescription(`Fun: daily, premium, rank, roulette, transfer
-            Info: help, remind
-            Music: loop, np, pause, play, queue, resume, skip, stop, volume
-            Rep: rep, reps
-            Work: coins, crime, work`);
+            .setDescription(`Fun: **daily, premium, rank, roulette, transfer**
+            Info: **help, remind**
+            Music: **loop, np, pause, play, queue, resume, skip, stop, volume**
+            Rep: **rep, reps**
+            Work: **coins, crime, work**`);
         message.channel.send(embed);
     }
 }
